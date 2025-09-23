2d8ad1363411282ca39533bb44fc7c26d5f79f55
NisheshTyagi <115863752+NisheshTyagi@users.noreply.github.com>
2025-09-23 17:06:16 +0530

    Added 17.series python file

--- 17. series/17.py
+++ 17. series/17.py
@@ -1,11 +1,9 @@
 n = int(input("Enter the range : "))
-if n%2 == 0:
-    natural = int(((n+1) * n/2))
-else:
-    natural = int((n+1) * (n//2)) + ((n+1)/2)
-sum = 0
+a=0
+b=0
 for i in range(1,n+1):
-    for j in range(1,i):
-        sum+=j
-
-print(sum)
+    for j in range(1,i+1):
+        a+=j
+    b+=a
+    a=0
+print(b)
